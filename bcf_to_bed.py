84baece2a9f7a9935a779277c7c638fba200c89e
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2018-01-12 17:01:59 -0500

    added arguments to argparse

--- bcf_to_bed.py
+++ bcf_to_bed.py
@@ -14,10 +14,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--vcf", help="file to process in Variant Call Format (required)")
     parser.add_argument("-s", "--sample", help="name of the sample to process in VCF file (required)")
+    parser.add_argument("--score", default="100", help="score for the entry (default: 100)")
+    parser.add_argument("--strand", default="+", help="strand for the entry (default: ")
     args = parser.parse_args()
 
     print("Convert BCF to BED files for each end of the translocation...\n")
     bcf_in = VariantFile(args.vcf)
+
+    for rec in bcf_in.fetch():
+        strand = "+"
+        score = "100"
 
 if __name__ == "__main__":
     main()
